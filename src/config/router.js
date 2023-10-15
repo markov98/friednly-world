b7c0e43e2438dd28930acfd89f5973035c4e0443
markov98 <74935630+markov98@users.noreply.github.com>
2023-10-16 02:29:21 +0300

    added homeController

--- src/config/router.js
+++ src/config/router.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
+const homeController = require('../controllers/home');
 
-router.get('/', (req, res) => {
-    res.send('Test');
-});
+router.use(homeController);
+
+router.get('*', (req, res) => {
+    res.redirect('/404');
+})
 
 module.exports = (app) => {
     app.use(router);
