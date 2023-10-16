ce97f0449c1d1060a84c7e6b58b19b4e3b1c0a35
markov98 <74935630+markov98@users.noreply.github.com>
2023-10-16 22:13:27 +0300

    added login functionality

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -6,6 +6,19 @@ router.get('/login', (req, res) => {
     res.render('users/login');
 });
 
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const token = await userService.login(email, password);
+
+        res.cookie('auth', token, { httpOnly: true });
+        res.redirect('/');
+    } catch (err) {
+        const errMessage = err.message;
+        res.status(404).render('users/login', { errMessage });
+    }
+});
+
 // Register Page
 router.get('/register', (req, res) => {
     res.render('users/register');
@@ -22,7 +35,7 @@ router.post('/register', async (req, res) => {
         await userService.register(email, password);
 
         const token = await userService.login(email, password);
-        res.cookie('auth', token);
+        res.cookie('auth', token, { httpOnly: true });
 
         res.redirect('/')
     } catch (err) {
