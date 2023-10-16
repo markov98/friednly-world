c9998203ee58779090592afb3905fce859f8bd34
markov98 <74935630+markov98@users.noreply.github.com>
2023-10-16 18:46:31 +0300

    added /register post endpoint

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,11 +1,23 @@
 const router = require("express").Router();
 
+//Login Page
 router.get('/login', (req, res) => {
     res.render('users/login');
 });
 
+// Register Page
 router.get('/register', (req, res) => {
     res.render('users/register');
+});
+
+router.post('/register', (req, res) => {
+    const {email, password, repeatPassword} = req.body;
+
+    try {
+
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 module.exports = router;
